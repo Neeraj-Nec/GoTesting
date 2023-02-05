22febfdfabca21ec279325249e181c1eae56100a
Neeraj Srivastava <Neeraj.Srivastava@nectechnologies.in>
2023-02-05 21:26:52 +0530

    Add html file

--- test/mainTestByTable_test.go
+++ test/mainTestByTable_test.go
@@ -13,8 +13,10 @@ func Test_IsPrime(t *testing.T) {
 		msg      string
 	}{
 		{"prime", 7, true, "7 is a prime number!"},
-		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2"},
-		{"not prime", -10, false, "Negative numbers are not prime, by definition!"},
+		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative number", -11, false, "Negative numbers are not prime, by definition!"},
 	}
 
 	for _, e := range primeTests {
